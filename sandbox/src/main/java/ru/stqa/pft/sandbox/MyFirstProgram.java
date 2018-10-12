1d0c34f1ccd4dfef1dd0c0a6986dde627a6cda9d
shipalkin <44064396+shipalkin@users.noreply.github.com>
2018-10-12 13:43:47 +0300

    Переменные

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,10 +1,13 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-System.out.println("Hello, world!");
+        int l = 10;
+        int s = l *l;
+        System.out.println("Hello, world!");
+        System.out.println("Площадь квардрата со стороной " + l + " = " + s);
 
-	}
+    }
 
 }
